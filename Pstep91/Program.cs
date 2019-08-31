f28e13bbea88c2daac51cb41110552bf303dcd7d
JayBista <52714504+jaybista@users.noreply.github.com>
2019-08-31 15:19:01 -0400

    Add files via upload

---
+++ Pstep91/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pstep91
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new Project9Entities();
+            var post = new Table_2()
+            {
+                email = "abc",
+               nickname = "sss",
+               password = "www",
+             };
+            context.Table_2.Add(post);
+            context.SaveChanges();
+        }
+    }
+}
